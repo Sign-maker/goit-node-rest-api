00a9e1523d318f3c34777e9d1031de84be6f638b
Sign-maker <muratovdv76@gmail.com>
2024-03-11 19:35:59 +0200

    contactSchema and env.example updated

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -2,8 +2,8 @@ import Joi from "joi";
 
 const createContactSchema = Joi.object({
   name: Joi.string().required(),
-  email: Joi.string().email(),
-  phone: Joi.string(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required(),
   favorite: Joi.boolean(),
 });
 
